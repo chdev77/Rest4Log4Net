9babb455e9c840af11bd616dda6715ff54603179
Corey Harbaugh <corey.harbaugh@gmail.com>
2016-06-23 22:49:37 -0500

    Initial working activity and error log sample

---
+++ Log4NetAppender/Log4NetAppender/Logging/Logger.cs
@@ -0,0 +1,19 @@
+﻿using log4net;
+using System;
+
+namespace Log4NetAppender
+{
+    public static class Logger
+    {
+        private static ILog _activity = LogManager.GetLogger("Activity");
+        private static ILog _exception = LogManager.GetLogger("Exception");
+        public static void Activity(string message)
+        {
+            _activity.Info(message);
+        }
+        public static void Exception(string message, Exception ex)
+        {
+            _exception.Error(message, ex);
+        }
+    }
+}
---
+++ Log4NetAppender/Log4NetAppender/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Log4NetAppender
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starting...press any key");
+            Console.ReadKey();
+
+            var msg = LoadMessages();
+
+            try
+            {
+                msg.ForEach(m => {
+                    Logger.Activity(m);
+                });
+                
+                throw new Exception("Ouch an exception!!!");
+            }
+            catch (Exception ex)
+            {
+                Logger.Exception("This is a test exception", ex);
+            }
+
+            Console.WriteLine("Done! press any key");
+            Console.ReadKey();
+        }
+
+        private static List<string> LoadMessages()
+        {
+            var msg = new List<string>();
+
+            msg.Add("This is my story");
+            msg.Add("My name is CSharp");
+            msg.Add("I live in your PC");
+            msg.Add("I love RAM and CPU");
+            msg.Add("I hate exceptions!");
+            msg.Add("I was slower when I was young");
+            msg.Add("Today I am very fast (sometimes)");
+            msg.Add("I want to be super duper fast, please!");
+            msg.Add("This is the end of my story");
+            msg.Add("Sincerely, CSharp");
+
+            return msg;
+        }
+    }
+}
