f2b5ab115034134c797f941f613f316cc1844e53
Corey Harbaugh <corey.harbaugh@gmail.com>
2016-06-23 23:17:40 -0500

    Renamed project and solution file

--- Log4NetAppender/Log4NetAppender/Properties/AssemblyInfo.cs
+++ Log4NetAppender/Log4NetAppender/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Log4NetAppender")]
+[assembly: AssemblyTitle("Log4NetTestLogger")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Log4NetAppender")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyProduct("Log4NetTestLogger")]
+[assembly: AssemblyCopyright("Corey Harbaugh ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
