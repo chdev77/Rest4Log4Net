7a4229a98a3aa580aa7ce3cdb4344c965d2a9a38
Corey Harbaugh <corey.harbaugh@gmail.com>
2016-06-23 23:28:37 -0500

    Added Rest4Log4NetAppender project skeleton

---
+++ Log4NetAppender/Rest4Log4NetAppender/RestAppender.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using log4net;
+using log4net.Core;
+using log4net.Appender;
+
+namespace Rest4Log4NetAppender
+{
+    public class RestAppender : AppenderSkeleton
+    {
+        public string PostUrl { get; set; }
+
+        protected override void Append(LoggingEvent loggingEvent)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
